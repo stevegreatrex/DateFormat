a5ca2fa7d015d4122a77ff44c559ab017c80324d
Steve Greatrex <stephen.greatrex@googlemail.com>
2014-02-04 11:19:38 +0000

    added missing jquery ui file

---
+++ dateFormat.jqueryui.js
@@ -0,0 +1,17 @@
+﻿(function (dateFormat, undefined) {
+    "use strict";
+
+	dateFormat.jqueryui = {
+        "day-of-month-1": "d",
+        "day-of-month-2": "dd",
+        "day-of-week-abbr": "D",
+        "day-of-week": "DD",
+        "month-1": "m",
+        "month-2": "mm",
+        "month-name-abbr": "M",
+        "month-name": "MM",
+        "year-2": "y",
+        "year-4": "yy"
+	};
+
+}(window.dateFormat));
